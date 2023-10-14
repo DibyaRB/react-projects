9fcba6d98353e0e7156033036cbb545636e87530
DibyaRB <dibyabhoidibya@gmail.com>
2023-10-15 00:06:44 +0530

    Initial Project Structure Commit

---
+++ gpt_drb/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Article, Brand, CTA} from './components';
+function App() {
+  return (
+    <div>
+      <h1>gpt 3</h1>
+    </div>
+  )
+}
+
+export default App;
