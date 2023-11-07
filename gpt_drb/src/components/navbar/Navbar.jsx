ddcfedd53269468552d04c95aa1a1f85e319e026
DibyaRB <dibyabhoidibya@gmail.com>
2023-11-07 20:38:42 +0530

    Made navbar responsive

--- gpt_drb/src/components/navbar/Navbar.jsx
+++ gpt_drb/src/components/navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css'
 import logo from '../../assets/logo.svg';
 
 //BEM -> Block Element Modifier as CSS naming convention
 
+
 function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="gpt3__navbar">
       <div className='gpt3__navbar-links'>
@@ -23,6 +25,28 @@ function Navbar() {
       <div className='gpt3__navbar-sign'>
         <p>Sign in</p>
         <button type='button'>Sign Up</button>
+      </div>
+      <div className='gpt3__navbar-menu'>
+        {toggleMenu
+          ? <RiCloseLine color="fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar_menu-container-links'>
+              <p><a href='#home'>Home</a></p>
+              <p><a href='#wgpt3'>What is GPT3</a></p>
+              <p><a href='#possibility'>Open AI</a></p>
+              <p><a href='#features'>Case Studies</a></p>
+              <p><a href='blog'>Library</a></p>
+
+              <div className='gpt3__navbar-menu_container-links-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign Up</button>
+      </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
